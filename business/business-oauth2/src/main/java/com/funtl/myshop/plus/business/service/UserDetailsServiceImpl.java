07a58b51eb98bd001f1e22eeddf8dfc6f03fe036
stepenshen <stepenshen@foxmail.com>
2020-03-06 07:56:28 +0800

    init commit

---
+++ business/business-oauth2/src/main/java/com/funtl/myshop/plus/business/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.funtl.myshop.plus.business.service;
+import com.google.common.collect.Lists;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.List;
+/**
+ * 自定义认证授权
+ * <p>
+ * Description:
+ * </p>
+ *
+ * @author Lusifer
+ * @version v1.0.0
+ * @date 2019-07-28 17:57:14
+ * @see com.funtl.myshop.plus.business.service
+ *
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "$2a$10$3YXWnCMhFAI9e8f24LBM0uYg9K3I6oRIMAlYSXEaitnWligmjNNUe";
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        List<GrantedAuthority> grantedAuthorities = Lists.newArrayList();
+        return new User(USERNAME, PASSWORD, grantedAuthorities);
+    }
+}
